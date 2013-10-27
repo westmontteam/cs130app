bcb0bd2df48b3680d03f1fa295e82c33eddb6db8
JamesByron <jameslbyron@gmail.com>
2013-10-27 02:40:35 -0700

    Added detail view for each point on the map

--- Course/src/edu/westmont/course/DistanceFinder.java
+++ Course/src/edu/westmont/course/DistanceFinder.java
@@ -9,12 +9,13 @@ public class DistanceFinder {
 
 	private boolean useMetric = true;
 	final private double imperialConversion = 3.28084;
-	private Double currentDistance = 0.0;
+	private double totalDistance = 0.0;
+	private double altitudeChange = 0.0;
 	final private DecimalFormat oneDecimal = new DecimalFormat("#,##0.0");
 	final private DecimalFormat noDecimal = new DecimalFormat("#,###");
 	private Location previousLocation = null;
 	private long startTime = 1;
-	private long currentTime = 1;
+	private String[] lastString = new String[5];
 
 	public String usingMetricOrImperial(){
 		if (useMetric)
@@ -26,12 +27,38 @@ public class DistanceFinder {
 		useMetric = !useMetric;
 	}
 
-	public Double getCurrentDistance(){
-		return currentDistance;
+	public Double getTotalDistance(){
+		return totalDistance;
 	}
 
-	public String getCurrentDistanceString(){
-		return getDistanceString(currentDistance);
+	private void addToAltitudeChange(double current){
+		altitudeChange += Math.abs(previousLocation.getAltitude()-current);
+	}
+
+	public Double getTotalAltitudeChange(){
+		if (useMetric) return altitudeChange;
+		return altitudeChange*imperialConversion;
+	}
+
+	public Double getAverageSpeed(Long timeMillis, double meters){
+		double hours = timeMillis.doubleValue()/3600000;
+		double distance = meters;
+		if (useMetric) distance /= 1000;
+		else distance = (distance*imperialConversion)/5280;
+		if (hours > 0) return distance/hours;
+		return 0.0;
+	}
+
+
+	public String getSpeedString(Double speed){
+		String output = oneDecimal.format(speed); 
+		if (useMetric) output += " kph";
+		else output += " mph";
+		return output;
+	}
+
+	public String getTotalDistanceString(){
+		return getDistanceString(totalDistance);
 	}
 
 	public String getDistanceString(Double dist){
@@ -51,27 +78,29 @@ public class DistanceFinder {
 	}
 
 	public void reset(){
-		currentDistance = 0.0;
+		altitudeChange = 0.0;
+		totalDistance = 0.0;
 		previousLocation = null;
 		startTime = 1;
-		currentTime = 1;
-	}
-
-	public void setDistance(Double dist){
-		currentDistance = dist;
 	}
 
 	public void addDistanceToLocation(Location loc){
+		double currentDistance = 0.0;
 		if (previousLocation != null) {
-			currentDistance += loc.distanceTo(previousLocation);
-			currentTime = loc.getTime();
+			addToAltitudeChange(loc.getAltitude());
+			currentDistance = loc.distanceTo(previousLocation);
+			totalDistance += currentDistance;
 		}
-		if (previousLocation == null)
-			startTime = loc.getTime();
+		else if (previousLocation == null) startTime = loc.getTime();
+		lastString = formatTitleString(loc.getTime(), currentDistance, loc.getAltitude());
 		previousLocation = loc;
 	}
+	
+	public String[] getLastString(){
+		return lastString;
+	}
 
-	public Double calculateAllDistances(Collection<Location> list){
+	/*public Double calculateAllDistances(Collection<Location> list){
 		Double distance = 0.0;
 		if (list.size() > 1) {
 			Iterator<Location> iterator = list.iterator();
@@ -83,14 +112,14 @@ public class DistanceFinder {
 			}
 		}
 		return distance;
-	}
+	}*/
 
-	public Long getElapsedTimeMillis(){
+	public Long getElapsedTimeMillis(long currentTime){
 		return currentTime-startTime;
 	}
 
-	public String getElapsedTimeString(){
-		Integer time = (Integer) getElapsedTimeMillis().intValue()/1000;
+	public String getElapsedTimeString(long currentTime){
+		Integer time = (Integer) getElapsedTimeMillis(currentTime).intValue()/1000;
 		Integer hours = time/3600;
 		time = time % 3600;
 		Integer minutes = time/60;
@@ -105,5 +134,14 @@ public class DistanceFinder {
 		return (output += input.toString());
 	}
 
-
+	public String[] formatTitleString(long currentTime, double currentDistance, double currentAltitude){
+		if (previousLocation != null) {
+			return new String[] {"Distance: " + getTotalDistanceString(), 
+					"Time: " + getElapsedTimeString(currentTime),
+					"Speed: " + getSpeedString(getAverageSpeed((currentTime-previousLocation.getTime()), currentDistance)), 
+					"Avg. Speed: " + getSpeedString(getAverageSpeed(getElapsedTimeMillis(currentTime), getTotalDistance())),
+					"Altitude: " + noDecimal.format(currentAltitude)};
+		}
+		return new String[] {"Distance: Start","Time: 0","Speed: 0","Avg. Speed: 0","Altitude: " + noDecimal.format(currentAltitude)};
+	}
 }
--- Course/src/edu/westmont/course/DrawMap.java
+++ Course/src/edu/westmont/course/DrawMap.java
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 //import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 //import android.R;
@@ -35,15 +36,17 @@ public class DrawMap extends FragmentActivity implements
 GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
 	private static final int GPS_ERRORDIALOG_REQUEST = 0;
 	GoogleMap myMap;
-	LocationClient myLocationClient;
-	int defaultZoom = 15;
-	boolean useDefaultZoom = true;
-	LocationChanger lc = new LocationChanger(40.715842,-74.006237);
-	DistanceFinder ranger = new DistanceFinder();
-	LinkedList<Location> listLocation = new LinkedList<Location>();
-	LatLngBounds.Builder boundsBuilder = LatLngBounds.builder();
-	boolean showCurrentLocation = false;
-	PositionsDataSource datasource;
+	protected LocationClient myLocationClient;
+	protected int defaultZoom = 15;
+	protected boolean useDefaultZoom = true;
+	protected LocationChanger lc = new LocationChanger(40.715842,-74.006237);
+	protected DistanceFinder ranger = new DistanceFinder();
+	protected String userDefinedName = "";
+	protected LinkedList<Location> listLocation = new LinkedList<Location>();
+	protected LatLngBounds.Builder boundsBuilder = LatLngBounds.builder();
+	protected boolean showCurrentLocation = false;
+	protected boolean moveCamera = true;
+	protected PositionsDataSource datasource;
 
 	/**
 	 * Initiates an instance of the class and if the mapping service is available
@@ -67,6 +70,7 @@ GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnect
 		String runName = intent.getStringExtra(MainActivity.RUN_NAME);
 		Toast.makeText(this, runName, Toast.LENGTH_SHORT).show();
 
+		userDefinedName = runName;
 		datasource = new PositionsDataSource(this);
 		datasource.open();
 		datasource.setRunName(runName);
@@ -84,10 +88,20 @@ GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnect
 	@Override
 	public boolean onOptionsItemSelected(android.view.MenuItem item) {
 		switch (item.getItemId()) {
+		case R.id.updateMapCamera:
+			moveCamera = !moveCamera;
+			if (moveCamera) item.setTitle(R.string.stay_put);
+			else {
+				item.setTitle(R.string.fly_to);
+				gotoCurrentLocation(false);
+			}
+			break;
 		case R.id.showCurrentLocation:
 			showCurrentLocation = !showCurrentLocation;
-			useDefaultZoom = true;
-			if (showCurrentLocation) item.setTitle(R.string.show_all);
+			if (showCurrentLocation) {
+				useDefaultZoom = true;
+				item.setTitle(R.string.show_all);
+			}
 			else item.setTitle(R.string.show_current);
 			gotoCurrentLocation(false);
 			break;
@@ -108,12 +122,11 @@ GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnect
 		}
 		return super.onOptionsItemSelected(item);
 	};
-	
+
 	private void changeMapType(int mapType){
 		useDefaultZoom = true;
 		myMap.setMapType(mapType);
 	}
-	
 
 	/**
 	 * Determines if the Google Play Services are available in the current operating environment and returns
@@ -142,6 +155,34 @@ GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnect
 			SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 			myMap = mapFrag.getMap();
 		}
+		if (myMap != null){
+			myMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+				@Override
+				public View getInfoWindow(Marker arg0) {
+					return null;
+				}
+
+				@Override
+				public View getInfoContents(Marker marker) {
+					View v = getLayoutInflater().inflate(R.layout.info_window, null);
+					TextView tvtitle = (TextView) v.findViewById(R.id.tv_title);
+					TextView tv1 = (TextView) v.findViewById(R.id.tv_text1);
+					TextView tv2 = (TextView) v.findViewById(R.id.tv_text2);
+					TextView tv3 = (TextView) v.findViewById(R.id.tv_text3);
+					TextView tv4 = (TextView) v.findViewById(R.id.tv_text4);
+					TextView tv5 = (TextView) v.findViewById(R.id.tv_text5);
+					String[] markerText = ranger.getLastString();
+					tvtitle.setText(marker.getTitle());
+					tv1.setText(markerText[0]);
+					tv2.setText(markerText[1]);
+					tv3.setText(markerText[2]);
+					tv4.setText(markerText[3]);
+					tv5.setText(markerText[4]);
+					return v;
+				}
+			});
+		}
 		return (myMap != null);
 	}
 
@@ -157,26 +198,27 @@ GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnect
 		if ((add) && (loc.getAccuracy() < 100)) {
 			datasource.createPosition(loc);
 			ranger.addDistanceToLocation(loc);
-			//Toast.makeText(this, ranger.getElapsedTimeString(), Toast.LENGTH_SHORT).show();
 			listLocation.add(loc);
 			addLatLngToMap(ll);
 		}
-		CameraUpdate update;
-		if (showCurrentLocation && useDefaultZoom) {
-			update = CameraUpdateFactory.newLatLngZoom(ll, defaultZoom);
-			useDefaultZoom = false;
+		if (moveCamera){
+			CameraUpdate update;
+			if (showCurrentLocation && useDefaultZoom) {
+				update = CameraUpdateFactory.newLatLngZoom(ll, defaultZoom);
+				useDefaultZoom = false;
+			}
+			else if (showCurrentLocation) update = CameraUpdateFactory.newLatLng(ll);
+			else {
+				LatLngBounds bounds = boundsBuilder.build();
+				update = CameraUpdateFactory.newLatLngBounds(bounds, 70);
+			}
+			myMap.animateCamera(update);
 		}
-		else if (showCurrentLocation) update = CameraUpdateFactory.newLatLng(ll);
-		else {
-			LatLngBounds bounds = boundsBuilder.build();
-			update = CameraUpdateFactory.newLatLngBounds(bounds, 70);
-		}
-		myMap.animateCamera(update);
 	}
 
 	protected void addLatLngToMap(LatLng ll){
 		MarkerOptions options = new MarkerOptions()
-		.title(ranger.getCurrentDistanceString())
+		.title(userDefinedName)
 		.position(ll);
 		myMap.addMarker(options);
 		drawLine();
